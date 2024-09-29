e133f40c90790e93f39da352c36336080b09b4b3
“weifeng2333” <2715673327@qq.com>
2024-09-29 19:35:39 +0800

    change file name

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ from bk_asr import BcutASR, JianYingASR, KuaiShouASR
 
 
 if __name__ == '__main__':
-    audio_file = "resources/低视力音乐助人者.mp3"
+    audio_file = "resources/test.mp3"
     asr = JianYingASR(audio_file)
     result = asr.run()
     result.to_srt()
-    print(result)
+    print(result.to_srt())
